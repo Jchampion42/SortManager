9183aa608b48fe115596aab7817b5efbaec48909
JChampion42 <jeffrey.champion42@gmail.com>
2022-06-29 16:41:34 +0100

    initial commit using previous bubble sort code

--- src/main/java/com/sparta/jeffrey/BubbleSort.java
+++ src/main/java/com/sparta/jeffrey/BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort implements Sorter{
 
     @Override
     public int[] sortArray(int[] arrayToSort) {
-        int tempNum=0;
+        int tempNum;
         for (int i=0 ; i< arrayToSort.length -1 ; i++){
             for (int j = 0 ; j<arrayToSort.length -1 ; j++){
                 if(arrayToSort[j]>arrayToSort[j+1]){
